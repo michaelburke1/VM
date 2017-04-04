a8dac0a4990f366ad2b77844b654583f98939991
michaelburke1 <michaeljburke18@gmail.com>
2017-04-04 16:30:51 -0400

    first step

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 /*
-Main program for the virtual memory project.
-Make all of your modifications to this file.
-You may add or rearrange any code or data as you need.
-The header files page_table.h and disk.h explain
-how to use the page table and disk interfaces.
-*/
+   Main program for the virtual memory project.
+   Make all of your modifications to this file.
+   You may add or rearrange any code or data as you need.
+   The header files page_table.h and disk.h explain
+   how to use the page table and disk interfaces.
+   */
 
 #include "page_table.h"
-#include "page_table.c"
+//#include "page_table.c"
 #include "disk.h"
 #include "program.h"
 
@@ -17,91 +17,114 @@ how to use the page table and disk interfaces.
 #include <errno.h>
 
 struct table {
+    char method[25];
     int size;
     int currFree;
     int elements[];
 };
 
 struct table *frameTable;
+struct disk *gDisk;
 
 void page_fault_handler( struct page_table *pt, int page )
 {
-    if (frameTable->currFree < frameTable->size - 1)
+    printf("curr %d size %d", frameTable->currFree, frameTable->size);
+    int frame;
+    int bits;
+    page_table_get_entry(pt, page, &frame, &bits);    
+
+    if (bits == 0)
     {
-        //load in
-        page_table_set_entry(pt, page, frameTable->currFree, PROT_READ);
-        disk_read(pt->fd, page, pt->physmem[frameTable->currFree * (pt->nframes)]);
-        frameTable->currFree++;
+        if (frameTable->currFree <= frameTable->size - 1)
+        {
+            printf("in if\n");
+            printf("page %d, currFree %d\n", page, frameTable->currFree);
+            //load in
+            page_table_set_entry(pt, page, frameTable->currFree, PROT_READ);
+
+            char *phys = page_table_get_physmem(pt);
+            int nframes = page_table_get_nframes(pt);
+            //        disk_read(gDisk, page, &phys[frameTable->currFree * nframes]);
+            //disk_read(pt->fd, page, pt->physmem[frameTable->currFree * (pt->nframes)]);
+            frameTable->currFree++;
+        }
     }
     else
     {
         //free then load in
 
         //writing
-        page_table_set_entry(pt, page, frameTable->currFree, PROT_READ|PROT_WRITE);
-        disk_read(pt->fd, page, pt->physmem[frameTable->currFree * (pt->nframes)]);
+        page_table_set_entry(pt, page, frame, PROT_READ|PROT_WRITE);
+
+        char *phys = page_table_get_physmem(pt);
+        int nframes = page_table_get_nframes(pt);
+        disk_read(gDisk, page, &phys[frame * nframes]);
 
         //freeing
         //x = randNum to free
-        disk_write(pt->fd, page, pt->physmem[0 * PAGE_SIZE]);
+        //   disk_write(gDisk, page, &phys[0 * PAGE_SIZE]);
+        //disk_write(pt->fd, page, pt->physmem[0 * PAGE_SIZE]);
 
     }
 
+    //page_table_set_entry(pt,page,page,PROT_READ|PROT_WRITE);
     printf("page fault on page #%d\n",page);
-	exit(1);
+    page_table_print(pt);
+    //exit(1);
 }
 
 int main( int argc, char *argv[] )
 {
-	if(argc!=5) {
-		printf("use: virtmem <npages> <nframes> <rand|fifo|lru|custom> <sort|scan|focus>\n");
-		return 1;
-	}
+    if(argc!=5) {
+        printf("use: virtmem <npages> <nframes> <rand|fifo|lru|custom> <sort|scan|focus>\n");
+        return 1;
+    }
 
-	int npages = atoi(argv[1]);
-	int nframes = atoi(argv[2]);
-	const char *program = argv[4];
+    int npages = atoi(argv[1]);
+    int nframes = atoi(argv[2]);
+    const char *program = argv[4];
 
     frameTable = malloc(sizeof(struct table) + nframes * sizeof(int));
     frameTable->currFree = 0;
     frameTable->size = nframes;
 
-	struct disk *disk = disk_open("myvirtualdisk",npages);
-	if(!disk) {
-		fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
-		return 1;
-	}
+    struct disk *disk = disk_open("myvirtualdisk",npages);
+    gDisk = disk;
+    if(!disk) {
+        fprintf(stderr,"couldn't create virtual disk: %s\n",strerror(errno));
+        return 1;
+    }
 
 
-	struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
-	if(!pt) {
-		fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
-		return 1;
-	}
+    struct page_table *pt = page_table_create( npages, nframes, page_fault_handler );
+    if(!pt) {
+        fprintf(stderr,"couldn't create page table: %s\n",strerror(errno));
+        return 1;
+    }
 
-	char *virtmem = page_table_get_virtmem(pt);
+    char *virtmem = page_table_get_virtmem(pt);
 
-	char *physmem = page_table_get_physmem(pt);
+    char *physmem = page_table_get_physmem(pt);
 
-	if(!strcmp(program,"sort")) {
-		sort_program(virtmem,npages*PAGE_SIZE);
+    if(!strcmp(program,"sort")) {
+        sort_program(virtmem,npages*PAGE_SIZE);
 
-	} else if(!strcmp(program,"scan")) {
-		scan_program(virtmem,npages*PAGE_SIZE);
+    } else if(!strcmp(program,"scan")) {
+        scan_program(virtmem,npages*PAGE_SIZE);
 
-	} else if(!strcmp(program,"focus")) {
-		focus_program(virtmem,npages*PAGE_SIZE);
+    } else if(!strcmp(program,"focus")) {
+        focus_program(virtmem,npages*PAGE_SIZE);
 
-	} else {
-		fprintf(stderr,"unknown program: %s\n",argv[3]);
-		return 1;
-	}
+    } else {
+        fprintf(stderr,"unknown program: %s\n",argv[3]);
+        return 1;
+    }
 
-	page_table_delete(pt);
-	disk_close(disk);
+    page_table_delete(pt);
+    disk_close(disk);
     free(frameTable);
 
-	return 0;
+    return 0;
 
 
 }
